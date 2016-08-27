1e43f423f1a71715b3b16f02841af3610e165edd
Said ID BAIH <said.idbaih@gmail.com>
2016-08-27 16:14:33 +0200

    CI

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :products, only: [:index, :create, :new]
+  get '/pages/home', :to => 'pages#home'
+
   devise_for :users
 
   root to: 'products#index'
--- test/integration/home_test.rb
+++ test/integration/home_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class HomeTest < ActionDispatch::IntegrationTest
   test "loads correctly" do
-    login_as users(:george)
     visit "/"
+    save_and_open_screenshot
     assert 200, page.status_code
     assert page.has_content?("Products")
   end
